1f8d5040b490635d3c1c07e6b62fc73109e3bc19
Travis <theller5567@gmail.com>
2025-12-05 13:58:04 -0500

    add Netlify site origin

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://emoji-memory-game-th.netlify.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
