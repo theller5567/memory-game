dada6e75d41ff8718a245a5d129286b057a5b7ba
Travis <theller5567@gmail.com>
2025-12-05 14:14:31 -0500

    server

--- src/server.js
+++ src/server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 4000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://emoji-memory-game-th.netlify.app/'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'https://emoji-memory-game-th.netlify.app',
+    'https://memory-game-0cz5.onrender.com'
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
